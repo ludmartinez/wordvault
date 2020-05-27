ddaa7988b0851e199a150c4c375d39ebaaac3ab6
Diego Martínez <diegomartinez8995@gmail.com>
2020-05-27 11:12:15 -0600

    feat: Implements Vuetifyjs, add custom components

---
+++ src/components/WvHeader.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-app-bar app color="teal" dark>
+    <div class="d-flex align-center">
+      <router-link :to="{ name: 'Home' }">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+      </router-link>
+
+      <h1 class="shrink mt-1 hidden-sm-and-down title">
+        WordVault
+      </h1>
+    </div>
+
+    <v-spacer></v-spacer>
+    <v-btn-toggle group mandatory>
+      <v-btn value="Home" to="/">
+        <span class="hidden-sm-and-down">Home</span>
+
+        <v-icon right>mdi-home</v-icon>
+      </v-btn>
+
+      <v-btn value="About" to="/about">
+        <span class="hidden-sm-and-down">About</span>
+
+        <v-icon right>mdi-information</v-icon>
+      </v-btn>
+    </v-btn-toggle>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "WvHeader"
+};
+</script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,14 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <hello-world />
 </template>
+
+<script>
+import HelloWorld from "../components/HelloWorld.vue";
+
+export default {
+  name: "About",
+  components: {
+    HelloWorld
+  }
+};
+</script>
