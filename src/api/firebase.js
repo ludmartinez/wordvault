54ef0c1af6ae22ead21f114260a9bd92aa0828ee
Diego Martínez <diegomartinez8995@gmail.com>
2020-05-28 14:04:38 -0600

    refactor: Change env variables configuration

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -1,18 +1,20 @@
 import * as firebase from "firebase/app";
 import "firebase/firestore";
+import "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: process.env.apiKey,
-  authDomain: process.env.authDomain,
-  databaseURL: process.env.databaseURL,
-  projectId: "wordvault",
-  storageBucket: process.env.storageBucket,
-  messagingSenderId: process.env.messagingSenderId,
-  appId: process.env.appId,
-  measurementId: process.env.measurementId
+  apiKey: process.env.VUE_APP_FB_API_KEY,
+  authDomain: process.env.VUE_APP_FB_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_FB_DB_URL,
+  projectId: process.env.VUE_APP_FB_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FB_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FB_MESSAGE_SENDER_ID,
+  appId: process.env.VUE_APP_FB_APP_ID,
+  measurementId: process.env.VUE_APP_FB_MEASUREMENT_ID
 };
 
 firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 const db = firebase.firestore();
 
--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
-import "./api/firebase";
+import firebase from "./api/firebase";
+import "firebase/analytics";
+
+firebase.analytics();
 
 Vue.config.productionTip = false;
 
